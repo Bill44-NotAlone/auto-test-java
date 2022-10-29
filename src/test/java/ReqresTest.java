ee7209fcb062353deda4bc52ad5c974961283db5
Bill44-NotAlone <denyc9@mail.ru>
2022-10-29 15:26:42 +0300

    Пост запрос

--- src/test/java/ReqresTest.java
+++ src/test/java/ReqresTest.java
@@ -1,18 +1,40 @@
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class ReqresTest {
 
+    public String baseUrl;
+
+    @BeforeClass
+    public void postInit(){
+        baseUrl = "https://reqres.in/";
+    }
+
     @Test
     @DisplayName("Получения списка пользователей 2 страници")
     public void listUsers(){
         given()
-                .baseUri("https://reqres.in/")
+                .baseUri(baseUrl)
                 .basePath("/api/users?page=2")
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Создание пользователя")
+    public void createUser(){
+        given()
+                .baseUri(baseUrl)
+                .basePath("api/users")
+                .contentType(ContentType.JSON)
+                .body("{\"name\":\"morpheus\", \"job\": \"leader\"}")
+                .when().post()
+                .then().statusCode(201)
+                .log().all();
     }
 }
