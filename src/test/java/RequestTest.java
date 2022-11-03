8d89225ceba79f8c745ecf9d0398dd1076077b99
Bill44-NotAlone <denyc9@mail.ru>
2022-11-03 21:26:33 +0300

    Post

--- src/test/java/RequestTest.java
+++ src/test/java/RequestTest.java
@@ -1,28 +1,16 @@
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
-public class ReqresTest {
+public class RequestTest {
 
     public String baseUrl;
 
     @BeforeClass
     public void postInit(){
         baseUrl = "https://reqres.in/";
-    }
-
-    @Test
-    @DisplayName("Получения списка пользователей 2 страници")
-    public void listUsers(){
-        given()
-                .baseUri(baseUrl)
-                .basePath("/api/users?page=2")
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then().statusCode(200);
     }
 
     @Test
