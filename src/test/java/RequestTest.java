3457d608bd6f78314efa7faa5685769589702d9c
Bill44-NotAlone <denyc9@mail.ru>
2022-11-03 21:21:28 +0300

    Get

--- src/test/java/RequestTest.java
+++ src/test/java/RequestTest.java
@@ -1,11 +1,10 @@
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
-public class ReqresTest {
+public class RequestTest {
 
     public String baseUrl;
 
@@ -23,18 +22,5 @@ public class ReqresTest {
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then().statusCode(200);
-    }
-
-    @Test
-    @DisplayName("Создание пользователя")
-    public void createUser(){
-        given()
-                .baseUri(baseUrl)
-                .basePath("api/users")
-                .contentType(ContentType.JSON)
-                .body("{\"name\":\"morpheus\", \"job\": \"leader\"}")
-                .when().post()
-                .then().statusCode(201)
-                .log().all();
     }
 }
